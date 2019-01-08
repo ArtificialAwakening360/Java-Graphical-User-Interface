40377c009ccde7256d5a65fcc3e2041356a5ab06
ArtificialAwakening360 <jrom2899@gmail.com>
2019-01-08 13:08:32 +0800

    JFrame included

--- BasicElements.java
+++ BasicElements.java
@@ -1,7 +1,14 @@
+import javax.swing.JFrame;
+import java.util.Scanner;
+
 public class BasicElements{
 	public static void main (String args[]){
+		JFrame myFrame = new JFrame();
+		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+		myFrame.setSize(900, 600);
 		System.out.println("basic java elements");
-
+		Scanner in = new Scanner (System.in);
 	}
 
 }
